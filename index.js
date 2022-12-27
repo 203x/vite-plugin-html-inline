84a4b31cf2705809a63dfcc59c2f0624d995164f
203X <system203x@gmail.com>
2022-12-27 15:27:06 +0800

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+import { parse } from 'node-html-parser'
+
+const htmlinline = () => {
+  return {
+    name: 'vite-plugin-html-inline',
+    transformIndexHtml(html, { path, filename, server, bundle, chunk }) {
+      const root = parse(html)
+
+      for (const key in bundle) {
+        if (Object.prototype.hasOwnProperty.call(bundle, key)) {
+          if (bundle[key].fileName.endsWith('.css')) {
+            for (const el of root.getElementsByTagName('link')) {
+              if (el.getAttribute('href').endsWith(bundle[key].fileName)) {
+                el.replaceWith(`<style>${bundle[key].source.trim()}</style>`)
+                delete bundle[key]
+              }
+            }
+          } else if (bundle[key].fileName.endsWith('.js')) {
+            for (const el of root.getElementsByTagName('script')) {
+              if (el.getAttribute('src').endsWith(bundle[key].fileName)) {
+                el.removeAttribute('src')
+                el.removeAttribute('crossorigin')
+                el.set_content(bundle[key].code.trim())
+                delete bundle[key]
+              }
+            }
+          }
+        }
+      }
+      return root.toString()
+    },
+  }
+}
+
+export default htmlinline
